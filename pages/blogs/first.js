d8e0bc3c74f2eb560ec7c96e779932d31a286a50
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2021-11-08 22:37:29 +0530

    nested routes

---
+++ pages/blogs/first.js
@@ -0,0 +1,4 @@
+const First = () => {
+  return <h1>First Blog page</h1>;
+};
+export default First;
---
+++ pages/blogs/second.js
@@ -0,0 +1,4 @@
+const Second = () => {
+  return <h1>Second blog page</h1>;
+};
+export default Second;
