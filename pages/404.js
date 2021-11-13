5834b326b2115f4b5fc0bcaf7082864d9aafb56f
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2021-11-13 18:16:02 +0530

    custome 404 page

---
+++ pages/404.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+const Home = () => {
+  return (
+    <>
+      <div id="main">
+        <div class="fzf">
+          <h1>Oops. 404!</h1>
+          <p>The page you are looking for does not exist.😞</p>
+          <Link href="/">
+            <a className="btn" data-wipe="Take Me Home">
+              Take Me Home
+            </a>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Home;
