0952f7b2dee793332d51eb790c162242deb5ecdd
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2021-11-22 12:50:05 +0530

    fallback: true, notfound when requested for the source which is not there

--- pages/blogs/[postId].js
+++ pages/blogs/[postId].js
@@ -1,4 +1,10 @@
+import { useRouter } from "next/router";
+
 const PostDetail = ({ post }) => {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <h1>Loading...</h1>;
+  }
   return (
     <>
       <div>
@@ -15,15 +21,25 @@ export async function getStaticPaths() {
   const blogs = await fetch("https://jsonplaceholder.typicode.com/posts");
   const response = await blogs.json();
 
-  const paths = response.map((post) => {
-    return {
-      params: { postId: `${post.id}` },
-    };
-  });
+  //   const paths = response.map((post) => {
+  //     return {
+  //       params: { postId: `${post.id}` },
+  //     };
+  //   });
 
   return {
-    paths: paths,
-    fallback: false,
+    paths: [
+      {
+        params: { postId: `1` },
+      },
+      {
+        params: { postId: `2` },
+      },
+      {
+        params: { postId: `3` },
+      },
+    ],
+    fallback: true,
   };
 }
 export async function getStaticProps(context) {
@@ -33,9 +49,17 @@ export async function getStaticProps(context) {
     `https://jsonplaceholder.typicode.com/posts/${params.postId}`
   );
 
+  const data = await postData.json()
+
+  if(!data.id){
+      return {
+          notFound : true
+      }
+  }
+
   return {
     props: {
-      post: await postData.json(),
+      post: data,
     },
   };
 }
